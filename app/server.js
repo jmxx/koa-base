21a4a3e7c13eced8d0300183ac20961d890a024b
Juan Manuel Reynoso <juan.manuel@ktc.com.mx>
2016-05-19 17:30:41 -0500

    Serves static content

--- app/server.js
+++ app/server.js
@@ -1,13 +1,18 @@
 import Koa    from 'koa';
 import path   from 'path';
 import views  from 'koa-views';
+import server from 'koa-static';
 
-const viewPaths = path.resolve(__dirname, '../resources/views');
+const viewPaths   = path.resolve(__dirname, '../resources/views');
+const publicPath  = path.resolve(__dirname, '../public');
+
 const app = new Koa();
 
 app.use(views(viewPaths, {
   extension: 'pug'
 }));
+
+app.use(server(publicPath));
 
 app.use(async (ctx) => {
   await ctx.render('index');
