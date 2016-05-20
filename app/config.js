dec9ae9a2bc52f591dda8854a3a5b1e69b4f0d45
Juan Manuel Reynoso <juan.manuel@ktc.com.mx>
2016-05-20 14:46:32 -0500

    Koa router added! With a tiny example

---
+++ app/config.js
@@ -0,0 +1,8 @@
+import path from 'path';
+
+export default {
+  paths : {
+    views   : path.resolve(__dirname, '../resources/views'),
+    public  : path.resolve(__dirname, '../public')
+  }
+};
--- app/server.js
+++ app/server.js
@@ -1,22 +1,31 @@
 import Koa    from 'koa';
-import path   from 'path';
 import views  from 'koa-views';
 import server from 'koa-static';
-
-const viewPaths   = path.resolve(__dirname, '../resources/views');
-const publicPath  = path.resolve(__dirname, '../public');
+import Router from 'koa-router';
+import config from './config';
 
 const app = new Koa();
+const router = new Router();
 
-app.use(views(viewPaths, {
+app.use(views(config.paths.views, {
   extension: 'pug'
 }));
 
-app.use(server(publicPath));
+app.use(server(config.paths.public));
 
-app.use(async (ctx) => {
-  await ctx.render('index');
-});
+router
+  .get('/', async (ctx) => {
+    await ctx.render('index', {
+      text: 'Koa'
+    });
+  })
+  .get('/:text', async (ctx) => {
+    await ctx.render('index', {
+      text: ctx.params.text
+    });
+  });
+
+app.use(router.routes());
 
 app.listen(process.env.PORT || 3000, () => {
   if (process.env.SIGNATURE) {
